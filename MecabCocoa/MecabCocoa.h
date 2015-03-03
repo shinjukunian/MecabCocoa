8771f997a8fed5109ff32d608c1093fdd7833fbe
morten <mbertz@Richelieu.local>
2015-03-03 12:16:20 +0900

    Initial Commit

---
+++ MecabCocoa/MecabCocoa.h
@@ -0,0 +1,19 @@
+//
+//  MecabCocoa.h
+//  MecabCocoa
+//
+//  Created by Morten Bertz on 3/3/15.
+//  Copyright (c) 2015 Morten Bertz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MecabCocoa.
+FOUNDATION_EXPORT double MecabCocoaVersionNumber;
+
+//! Project version string for MecabCocoa.
+FOUNDATION_EXPORT const unsigned char MecabCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MecabCocoa/PublicHeader.h>
+
+
