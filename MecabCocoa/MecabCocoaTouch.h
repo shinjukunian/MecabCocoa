861cac0e38be2c114e6c7b61138690ecfed4205c
morten <mbertz@Richelieu.local>
2015-04-03 23:42:54 +0900

    added new target

--- MecabCocoa/MecabCocoaTouch.h
+++ MecabCocoa/MecabCocoaTouch.h
@@ -14,6 +14,11 @@ FOUNDATION_EXPORT double MecabCocoaVersionNumber;
 //! Project version string for MecabCocoa.
 FOUNDATION_EXPORT const unsigned char MecabCocoaVersionString[];
 
+#import "MecabToken.h"
+#import "NSString+Mecab.h"
+#import "MecabTokenizer.h"
+
+
 // In this header, you should import all the public headers of your framework using statements like #import <MecabCocoa/PublicHeader.h>
 
 
